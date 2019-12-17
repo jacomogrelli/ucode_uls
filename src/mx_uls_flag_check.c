bedf76f0e18f8bbb294bd592876765f9b2881769
jacomogrelli <nechaeff@gmail.com>
2019-12-17 18:15:50 +0200

    flag_check

--- src/mx_uls_flag_check.c
+++ src/mx_uls_flag_check.c
@@ -1,19 +1,27 @@
 #include "uls.h"
 
+/* функция принимает argc, argv, проверяет, входят ли флаги в множество
+константы FLAGS и возвращает количество принимаемых аргументов, которые
+являются наборами флагов или прерыванием флагов, для того, что бы начать
+считывать аргументы сразу после них. Дальше использовать аргумент argv[res]*/
+
 static void uls_err_illegal_option(char c);
 
-void mx_flag_check(int argc, char **argv) {
-    if (argc == 1)
-        return;
-    for (int i = 1; i < argc; i++) {
-        if (argv[i][0] != '-')
-            return;
-        for (int j = 1; argv[i][j]; j++) {
-            if (mx_get_char_index(FLAGS, (argv[i][j])) == -1)
-                uls_err_illegal_option(argv[i][j]);
+int mx_flag_check(int argc, char **argv) {
+    int res = 1;
+
+    for (int i = 1; i < argc && argv[i][0] == '-'; i++) {
+        if (mx_strcmp(argv[i], "--") == 0)
+            return res + 1;
+        if (argv[i][0] == '-') {
+            res++;
+            for (int j = 1; argv[i][j]; j++) {
+                if (mx_get_char_index(FLAGS, (argv[i][j])) == -1)
+                    uls_err_illegal_option(argv[i][j]);
+            }
         }
     }
-    return;
+    return res;
 }
 
 static void uls_err_illegal_option(char c) {
