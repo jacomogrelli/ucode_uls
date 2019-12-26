3f5466d66887dafba6d2b9c92f8d8814e209a4dd
jacomogrelli <nechaeff@gmail.com>
2019-12-26 20:06:53 +0200

    Function sort linked list in ascii order

---
+++ src/mx_ascii_sort_list.c
@@ -0,0 +1,16 @@
+    #include "libmx.h"
+
+    t_list *mx_ascii_sort_list(t_list *lst) {
+    void *tmp;
+
+	if (!lst)
+        return NULL;
+    for (t_list *i = lst; i->next; i = i->next)
+        for (t_list *j = i->next; j; j = j->next)
+            if (mx_strcmp(i->data, j->data) > 0) {
+            	tmp = i->data;
+                i->data = j->data;
+                j->data = tmp;
+            }
+    return lst;
+}
