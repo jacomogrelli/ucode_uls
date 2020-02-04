bf52fa765ffc7a4bdcfdba2932ed2d3437c1c1b6
An <46691148+ailchuk@users.noreply.github.com>
2020-02-04 16:35:16 +0200

    Update mx_output_folders.c

--- src/mx_output_folders.c
+++ src/mx_output_folders.c
@@ -3,12 +3,14 @@
 void mx_output_folders(t_list *out, int *flags) {
     if (flags[mx_get_char_index(FLAGS, 'l')])
         mx_print_l(out, flags);
-    else {
-        for (;out; out = out->next) {
-            mx_printstr(out->data);
-            mx_printstr(" ");
-            mx_printstr("\n");
-        }
-    }
+    if (flags[mx_get_char_index(FLAGS, 'F')])
+        flag_F(out);
+//     else {
+//         for (;out; out = out->next) {
+//             mx_printstr(out->data);
+//             mx_printstr(" ");
+//             mx_printstr("\n");
+//         }
+//     }
     free(out);
 }
