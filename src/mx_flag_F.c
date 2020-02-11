8bf40318c90c7db6c44c69e4283ac03f5af7349e
jacomogrelli <nechaeff@gmail.com>
2020-02-11 16:41:01 +0200

    fixed priority in -F

--- src/mx_flag_F.c
+++ src/mx_flag_F.c
@@ -3,10 +3,10 @@
 char *mx_flag_F(char *name, struct stat buf) {
     if (MX_ISDIR(buf.st_mode))
         name = mx_strjoin(name, "/"); // dirs
-    else if (MX_ISEXEC(buf.st_mode))
-        name = mx_strjoin(name, "*"); // exec
     else if (MX_ISLNK(buf.st_mode))
         name = mx_strjoin(name, "@"); // links /usr/sbin/
+    else if (MX_ISEXEC(buf.st_mode))
+        name = mx_strjoin(name, "*"); // exec
     else if (MX_ISFIFO(buf.st_mode))
         name = mx_strjoin(name, "|"); // if the file is a FIFO
     else if (MX_ISSOCK(buf.st_mode))
