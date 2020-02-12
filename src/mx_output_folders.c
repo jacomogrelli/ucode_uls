1235a0b9fe200a5ae4f6bcc5dc164f139da1b9be
Andrii Ilchuk <ailchuk@e2r6p8.unit.ua>
2020-02-12 17:42:33 +0200

    aud

--- src/mx_output_folders.c
+++ src/mx_output_folders.c
@@ -1,36 +1,40 @@
 #include "uls.h"
+static void error_dir (t_list *dirs, t_uls_out *all) {
+    mx_error_pd(dirs->data);
+    all->err = true;
+    if (dirs->next)
+        mx_printchar('\n');
+}
+
+static void p_dir(t_uls_out *dlist, int *flags, DIR *dp) {
+    if ((flags[mx_get_char_index(FLAGS, 'l')] ||
+         flags[mx_get_char_index(FLAGS, 'o')]) && dlist->Dlist)
+         mx_output_total(dlist->Dlist);
+    mx_output_files(dlist->Dlist, flags);
+    if (dlist->next->next)
+        mx_printchar('\n');
+        closedir(dp);
+}
 
 void mx_output_folders(t_uls_out *all, int *flags, int ac, int flag) {
-    t_list *dirs = all->D;
     t_uls_out *dlist = all;
     DIR *dp;
     bool mark = false;
 
     if (!all->D)
         return;
-    while (dirs) {
+    if (all->D && all->F)
+        mx_printchar('\n');
+    for (t_list *dirs = all->D; dirs != NULL; dirs = dirs->next) {
         if (ac != flag && ac != flag + 1 && !mark)
             mx_print_dname(dirs->data);
         if (mark)
             mx_print_dname(dirs->data);
         mark = true;
-        if (!(dp = opendir(dirs->data))) {
-            mx_error_pd(dirs->data);
-            all->err = true;
-            if (dirs->next)
-                mx_printchar('\n');
-        }
-        else {
-            if ((flags[mx_get_char_index(FLAGS, 'l')] ||
-                flags[mx_get_char_index(FLAGS, 'o')]) && dlist->Dlist) {
-                    mx_output_total(dlist->Dlist);
-            }
-            mx_output_files(dlist->Dlist, flags);
-            if (dlist->next->next)
-                mx_printchar('\n');
-            closedir(dp);
-        }
-        dirs = dirs->next;
+        if (!(dp = opendir(dirs->data)))
+            error_dir(dirs, all);
+        else
+            p_dir(dlist, flags, dp);
         dlist = dlist->next;
     }
 }
